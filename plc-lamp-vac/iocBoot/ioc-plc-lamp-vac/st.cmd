073e2568928fdc68dd3155b2b974f74b953110af
bosum123 <bosum123@slac.stanford.edu>
2021-10-28 13:03:46 -0700

    Working LAMP Vacuum code with original LAMP configuration of 15 turbos (no KTOOF), and ECAT bridge communication. This was the working code for 10/21 for x454 and x471

--- plc-lamp-vac/iocBoot/ioc-plc-lamp-vac/st.cmd
+++ plc-lamp-vac/iocBoot/ioc-plc-lamp-vac/st.cmd
@@ -5,7 +5,7 @@
 
 epicsEnvSet("ADS_IOC_TOP", "$(TOP)" )
 
-epicsEnvSet("ENGINEER", "adpai" )
+epicsEnvSet("ENGINEER", "bosum123" )
 epicsEnvSet("LOCATION", "PLC:LAMP:VAC" )
 epicsEnvSet("IOCSH_PS1", "$(IOC)> " )
 epicsEnvSet("ACF_FILE", "$(ADS_IOC_TOP)/iocBoot/templates/unrestricted.acf")
